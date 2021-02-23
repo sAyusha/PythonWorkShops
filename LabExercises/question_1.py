fc1d02ec440c3886aae832591fd7ceb3844b7909
User <ayushastha69@gmail.com>
2021-02-23 10:02:37 +0545

    Initial commit

---
+++ LabExercises/question_1.py
@@ -0,0 +1,7 @@
+#WAP that takes three numbers and prints their sum. Every number is given on a separate line.
+a=int(input('Enter 1st number:'))
+b=int(input('Enter 2nd number:'))
+c=int(input('Enter 3rd number:'))
+sum=a+b+c
+print('the sum is', sum)
+
---
+++ LabExercises/question_2.py
@@ -0,0 +1,6 @@
+#WAP that reads the length of the base and the height of a right-angled triangle.
+
+length=int(input('Enter the length:'))
+height=int(input('Enter the height:'))
+area=(length*height)//2
+print('the area of triangle is',area)
