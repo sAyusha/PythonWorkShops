d9286fd27406ea62f774d301365490caf41b7b43
User <ayushastha69@gmail.com>
2021-03-02 10:14:11 +0545

    desk requirement

---
+++ LabExercises/question_5.py
@@ -0,0 +1,10 @@
+''' School decided to replace desks in 3 classrooms. Each desk sits 2 students. Given the number of students in each
+class, print the smallest number of desk that can be purchased.
+'''
+class1=int(input("Enter the number of student in 1st class:"))
+class2=int(input("Enter the number of student in 2nd class:"))
+class3=int(input("Enter the number of student in 3rd class:"))
+
+total_students=(class1+class2+class3)
+desk=total_students//2
+print("The total number of desk required is:",desk)
