3bde9fb60aa1187abdc59ea6b3dfc9d95d13d6ac
User <ayushastha69@gmail.com>
2021-03-16 09:52:07 +0545

    positive integer

---
+++ LabExercises/question_9.py
@@ -0,0 +1,7 @@
+'''WAP to find sum of the first n positive integers.'''
+#n=int(input('Entetr the positive number:'))
+#sum=(n+(n+1))/2
+#print(sum)
+list=[20,25,30,35,40,45,50]
+total=sum(list)
+print(f"the sum of given positive integer is {total}")
