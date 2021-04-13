0ac57ca4e74e322c39b4b12e04aaad0e253d1a70
sAyusha <ayushastha69@gmail.com>
2021-04-13 09:23:57 +0545

    simple program

---
+++ Lab2/que_3.py
@@ -0,0 +1,6 @@
+'''check whether the user input number is even or odd and display it to user.'''
+num=int(input('Enter the number:'))
+if num%2==0:
+    print('the number is even.')
+else:
+    print("the number is odd.")
