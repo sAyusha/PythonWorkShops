f24f6db4db510f6443f4d6505ff0c798accd3051
sAyusha <ayushastha69@gmail.com>
2021-04-13 09:39:57 +0545

    simple prog

---
+++ Lab2/que_5.py
@@ -0,0 +1,13 @@
+'''For given integer x, print 'True' if it is positive,
+ print 'False' if it is negative and print 'Zero' if it is 0.'''
+x=int(input('Enter the integer:'))
+if x>0 :
+    true="positive"
+    print('true')
+elif x<0:
+    false="negative"
+    print('false')
+else:
+    print('number is zero.')
+
+
