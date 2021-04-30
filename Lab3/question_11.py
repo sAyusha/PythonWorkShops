8cc4d57107728b65f954ee6347a932d03bd78e73
sAyusha <78839653+sAyusha@users.noreply.github.com>
2021-04-30 16:39:34 +0545

    Delete question_11.py

--- Lab3/question_11.py
+++
@@ -1,9 +0,0 @@
-'''WAP to find the factorial of a number using function.'''
-def factorial(num):
-    if num<0:
-        return 'number cannot be negative.'
-    elif num==1 or num==0:
-        return 1
-    else:
-        return num*factorial(num-1)
-print(factorial(5))
